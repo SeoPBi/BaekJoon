176b74f9413e7b22280116f4c1b291afa4a1299a
SeoPBi <aktms252@naver.com>
2023-10-23 12:45:40 +0900

    백준 재시작

---
+++ src/main/java/com/example/beakjoon/BeakJoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.beakjoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeakJoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeakJoonApplication.class, args);
+    }
+
+}
