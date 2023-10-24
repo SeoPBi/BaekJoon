73540bbaed7beff3e12271d3645b3f1e7637dc8a
SeoPBi <aktms252@naver.com>
2023-10-24 16:53:37 +0900

    백준 기초 풀기

---
+++ src/main/java/Step1/B11382.java
@@ -0,0 +1,21 @@
+package Step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class B11382 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        sc.close();
+        System.out.println(a + b + c);
+
+    }
+}
---
+++ src/main/java/Step1/B2753.java
@@ -0,0 +1,19 @@
+package Step1;
+
+import java.util.Scanner;
+
+public class B2753 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        if (input % 4 == 0) {
+            if (input % 400 == 0) System.out.println("1");
+            else if (input % 100 == 0) System.out.println("0");
+            else System.out.println("1");
+        } else {
+            System.out.println("1");
+        }
+    }
+}
