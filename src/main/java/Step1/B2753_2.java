b620973aa6b51ec2129c2ae1b81e945da603b6e0
SeoPBi <aktms252@naver.com>
2023-10-25 16:58:42 +0900

    2753 맞음

---
+++ src/main/java/Step1/B2753_2.java
@@ -0,0 +1,19 @@
+package Step1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B2753_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(br.readLine());
+
+        if (input % 4 == 0) {
+            if (input % 400 == 0) System.out.println("1");
+            else if (input % 100 != 0) System.out.println("1");
+            else System.out.println("0");
+        } else System.out.println("0");
+    }
+}
