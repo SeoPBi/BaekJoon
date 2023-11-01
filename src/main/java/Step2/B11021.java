8d801a5733daf3818c7f4142ada40a02d7b78d17
SeoPBi <aktms252@naver.com>
2023-11-01 20:59:22 +0900

    11021번 맞음

--- src/main/java/Step2/B11021.java
+++ src/main/java/Step2/B11021.java
@@ -1,20 +1,18 @@
 package Step2;
 
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
-
 import java.io.*;
 
-public class B15552 {
+public class B11021 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int number = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 1; i <= number; i++) {
             String[] input = br.readLine().split(" ");
             int sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
-            bw.write(String.valueOf(sum));
+            bw.write("Case #" + i + ": " + String.valueOf(sum));
             bw.newLine();
         }
         bw.flush();
