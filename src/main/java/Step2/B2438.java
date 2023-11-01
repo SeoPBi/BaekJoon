3e25b909db83fdae21c429b55a81919efe481131
SeoPBi <aktms252@naver.com>
2023-11-01 21:22:15 +0900

    2438문제 풀음

---
+++ src/main/java/Step2/B2438.java
@@ -0,0 +1,21 @@
+package Step2;
+
+import java.io.*;
+
+public class B2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= input; i++) {
+            for (int j = 1; j <= i; j++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
