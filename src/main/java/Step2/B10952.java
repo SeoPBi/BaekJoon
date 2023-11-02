164274022840a0abb8b9408f103fdd529ef749ac
SeoPBi <aktms252@naver.com>
2023-11-02 23:20:10 +0900

    10952번 문제 푸는중

---
+++ src/main/java/Step2/B10952.java
@@ -0,0 +1,19 @@
+package Step2;
+
+import java.io.*;
+
+public class B10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input1 = Integer.parseInt(br.readLine());
+        int input2 = Integer.parseInt(br.readLine());
+
+        if (input1 != 0 && input2 != 0) {
+            for (int i = 0; i < ; i++) {
+
+            }
+        }
+    }
+}
